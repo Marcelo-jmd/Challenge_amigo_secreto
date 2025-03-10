b18cf519615bcba9f6e00de76bc92c9dc2cefb2c
Marcelo-jmd <u19207181@utp.edu.pe>
2025-03-09 20:06:08 -0500

    Subir Challenge
    Incluye modificaciones del Java Script

---
+++ app.js
@@ -0,0 +1,43 @@
+/* El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. 
+Aquí deberás desarrollar la lógica para resolver el problema.
+*/
+let amigos =[];
+
+
+function agregarAmigo(){
+    let amigoIngresado = document.getElementById("amigo").value;
+    let listaMostrar = document.getElementById("listaAmigos");
+    listaMostrar.innerHTML="";
+    if (amigoIngresado ==""){
+        alert("Por favor, inserte un nombre.")
+    }else{
+        amigos.push(amigoIngresado);
+        console.log(amigos);
+        for (i=0;i<amigos.length;i++){
+            let nuevoAmigo = document.createElement("li"); //Crea nuevo <li>
+            nuevoAmigo.textContent = amigos[i]; // Se le asigna valor
+            listaMostrar.appendChild(nuevoAmigo); //Se agrega el nuevo elemento a la <ul>
+            
+        }
+        
+        document.getElementById("amigo").value="";
+    }
+}
+
+function sortearAmigo(){
+    let resultadoMostrar = document.getElementById("resultado");
+    resultadoMostrar.innerHTML="";
+    let tamañoArreglo = amigos.length;
+    console.log(tamañoArreglo);
+    let numAleatorio =Math.floor(Math.random()*tamañoArreglo);
+    console.log(numAleatorio);
+    let amigoSecreto = document.createElement("li");
+
+    if (tamañoArreglo === 0){
+        alert("No ingreso ningún nombre.")
+    }else{
+        amigoSecreto.textContent = amigos[numAleatorio];
+        console.log(amigoSecreto)
+        resultadoMostrar.appendChild(amigoSecreto);
+    }
+}
